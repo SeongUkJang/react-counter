2d08346211d94ecd2a5c740339653a97745d01bc
SeongUkJang <seonguk94@gmail.com>
2025-03-24 10:03:01 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Controller from './assets/components/Controller'
+import Viewer from './assets/components/Viewer'
+import './App.scss'
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount]= useState(0)
+  const buttonValues=[-100,-10,-1,1,10,100]
+  const onClickButton=(value)=>{
+    setCount(count + value)
+  }
+  return (
+    <div className='App'>
+      <h1>Simple Counter</h1>
+
+      <section>
+      <Viewer count={count}/>
+
+      </section>
+
+      <section>
+      <Controller buttonValues={buttonValues} onClickButton={onClickButton}/>
+
+      </section>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/Viewer.jsx
@@ -0,0 +1,11 @@
+    import React from 'react'
+
+    const Viewer = ({count}) => {
+    return (
+        <div>
+        현재 Count : <span>{count}</span>
+        </div>
+    )
+    }
+
+    export default Viewer
